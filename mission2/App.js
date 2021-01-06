b0aaa4352c45da012fb3c76f4db805743e830093
jioun.lee.dev <jioun.lee@nts-corp.com>
2021-01-06 21:11:50 +0900

    mission#2: 강의 내용 토대로 복기

---
+++ mission2/App.js
@@ -0,0 +1,45 @@
+import TodoCount from "./TodoCount.js";
+import TodoInput from "./TodoInput.js";
+import TodoList from "./TodoList.js";
+
+import { getItem, setItem } from "./localStorage.js";
+
+const TODOS_STORAGE_KEY = "todos";
+export default function App({ $app }) {
+  this.$app = $app;
+  this.state = getItem(TODOS_STORAGE_KEY, []);
+
+  this.components = [
+    new TodoInput({
+      onAddTodo: (text) => {
+        const nextState = [
+          ...this.state,
+          {
+            text,
+            isCompleted: false,
+          },
+        ];
+
+        this.setState(nextState);
+      },
+      $app,
+    }),
+    new TodoList({
+      $app,
+      initialState: this.state,
+    }),
+    new TodoCount({
+      $app,
+      initialState: this.state,
+    }),
+  ];
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    setItem(TODOS_STORAGE_KEY, nextState);
+
+    this.components.forEach(
+      (component) => component.setState && component.setState(this.state)
+    );
+  };
+}
---
+++ mission2/TodoInput.js
@@ -0,0 +1,25 @@
+export default function TodoInput({ $app, onAddTodo }) {
+  const $target = document.createElement("input");
+  $target.className = "TodoInput";
+  this.$target = $target;
+  $app.appendChild($target);
+
+  this.onAddTodo = onAddTodo;
+
+  this.render = () => {};
+
+  this.initEvent = () => {
+    this.$target.addEventListener("keydown", (e) => {
+      const { value } = e.target;
+
+      if (value.length > 0 && e.key === "Enter") {
+          // TODO: data validation 추가
+        this.onAddTodo(value);
+        this.$target.value = "";
+      }
+    });
+  };
+
+  this.render();
+  this.initEvent();
+}
